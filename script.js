c34ba5f497bcc5d15dc358f62cfe0123c780e672
leohuang95 <leohuang95@gmail.com>
2021-07-02 13:33:30 +0800

    【Feat】 - Homepage navbar scroll, beauty code

--- script.js
+++ script.js
@@ -104,43 +104,8 @@ function enableScrolling() {
     window.onscroll = function() {};
 }
 
-(function($) {
-    $.fn.arctic_scroll = function(options) {
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        };
-        var options = $.extend(defaults, options);
-
-        options.elem.click(function(event) {
-            event.preventDefault();
-            var offset = $(this).attr("data-offset") ?
-                $(this).attr("data-offset") :
-                false,
-                position = $(this).attr("data-position") ?
-                $(this).attr("data-position") :
-                false;
-            if (offset) {
-                var toMove = parseInt(offset);
-                $("html,body")
-                    .stop(true, false)
-                    .animate({ scrollTop: $(this.hash).offset().top + toMove }, options.speed);
-            } else if (position) {
-                var toMove = parseInt(position);
-                $("html,body")
-                    .stop(true, false)
-                    .animate({ scrollTop: toMove }, options.speed);
-            } else {
-                $("html,body")
-                    .stop(true, false)
-                    .animate({ scrollTop: $(this.hash).offset().top }, options.speed);
-            }
-        });
-    };
-})(jQuery);
-
-$(function() {
-    $(".scroller").arctic_scroll({
-        speed: 600
-    });
-});
+function scrolltoID(id, speed = 800) {
+    $('html, body').animate({
+        scrollTop: $('#' + id).offset().top
+    }, speed);
+}
